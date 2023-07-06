7e9cfd1f2fc7769bccbbc297a139dfaeb609fe44
sfirdous <126231373+sfirdous@users.noreply.github.com>
2023-07-06 16:48:53 +0530

    Add files via upload

---
+++ project.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score
